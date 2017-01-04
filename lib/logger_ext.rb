99d91da1a76b44d0e9f6ba86643d47f98133240b
eno <eno@open-lab.org>
2017-01-04 19:34:46 +0100

    Refactored logger_ext
    We no longer use a separate formatting function. Instead we just build
    an ExtendedMessage object, which somehow can also act as a String (i.e.
    it has a to_s method :) but for formatting purposes holds all attributes
    of the original logging call

--- lib/logger_ext.rb
+++ lib/logger_ext.rb
@@ -2,56 +2,71 @@ require "logger_ext/version"
 require "logger"
 
 class Logger
+  class ExtendedMessage
+    attr_reader :message
+    attr_reader :args
+
+    def initialize(message, *args)
+      @message = message
+      @args    = args
+    end
+
+    def to_s
+      if args.length == 0
+        message
+      else
+        s = "#{message}: #{args.shift.inspect}"
+        args.each_with_index do |arg, idx|
+          next if idx == 0
+          s += ", #{arg.inspect}"
+        end
+        s
+      end
+    end
+  end
+
   private
 
-  def log_with_options(severity, *args, &block)
+  def add_with_options(severity, *args, &block)
     return unless level <= severity
 
     if block
       # Note: this will raise an error if args is not empty
       add severity, nil, *args, &block
     else
-      add severity, format_message_with_options(*args), nil
+      add severity, ExtendedMessage.new(*args), nil
     end
-  end
-
-  def format_message_with_options(message, *args)
-    message += ": #{args.shift.inspect}" unless args.empty?
-    args.each_with_index do |arg, idx|
-      message += ", #{arg.inspect}"
-    end
-    message
   end
 
   public
 
   # Log a +DEBUG+ message.
   def debug(*args, &block)
-    log_with_options(DEBUG, *args, &block)
+    add_with_options(DEBUG, *args, &block)
   end
 
   # Log an +INFO+ message.
   def info(*args, &block)
-    log_with_options(INFO, *args, &block)
+    add_with_options(INFO, *args, &block)
   end
 
   # Log a +WARN+ message.
   def warn(*args, &block)
-    log_with_options(WARN, *args, &block)
+    add_with_options(WARN, *args, &block)
   end
 
   # Log an +ERROR+ message.
   def error(*args, &block)
-    log_with_options(ERROR, *args, &block)
+    add_with_options(ERROR, *args, &block)
   end
 
   # Log a +FATAL+ message.
   def fatal(*args, &block)
-    log_with_options(FATAL, *args, &block)
+    add_with_options(FATAL, *args, &block)
   end
 
   # Log an +UNKNOWN+ message.
   def unknown(*args, &block)
-    log_with_options(UNKNOWN, *args, &block)
+    add_with_options(UNKNOWN, *args, &block)
   end
 end
